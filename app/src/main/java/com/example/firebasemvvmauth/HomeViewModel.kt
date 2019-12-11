69ab800b16dd761e8e558f8553126b4a051ef165
chris <chrisribia@gmail.com>
2019-12-11 15:38:06 +0300

    Drawer layout

---
+++ app/src/main/java/com/example/firebasemvvmauth/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.firebasemvvmauth
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/firebasemvvmauth/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.firebasemvvmauth
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
